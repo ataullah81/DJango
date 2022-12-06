a9632b90e741fca12e5d20ba29a78a76085e95a8
Ataullah Behesti <ataullah.behesti@gmail.com>
2022-12-06 15:45:54 +0200

    Django model

---
+++ Student-management-system/informationmanagementsystem/studentapp/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+# Create your models here.
+class Student(models.Model):
+    std_id = models.CharField(max_length=100)
+    std_name = models.CharField(max_length=200)
+    std_email = models.EmailField()
+    std_contact_number = models.CharField(max_length=15)
+
+    class Meta:
+        db_table = "student"
---
+++ Student-management-system/informationmanagementsystem/studentapp/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render, redirect
+
+from studentapp.forms import StudentForm
+from studentapp.models import Student
+
+
+# Create your views here.
+def addStudent(request):
+    if request.method == "POST":
+        form = StudentForm(request.POST)
+
+        if form.is_valid():
+            try:
+                form.save()
+                return redirect("/all-students")
+            except:
+                pass
+    else:
+        form = StudentForm()
+    return render(request, 'index.html', {"form": form})
+
+
+def getAllData(request):
+    students = Student.objects.all()
+    return render(request, "show.html", {"students": students})
+
+
+def edit(request, id):
+    student = Student.objects.get(id=id)
+    return render(request, "edit.html", {"student": student})
+
+
+def update(request, id):
+    student = Student.objects.get(id=id)
+    form = StudentForm(request.POST, instance=student)
+    if form.is_valid():
+        form.save()
+        return redirect("/all-students")
+    return render(request, "edit.html", {"student": student})
+
+
+def delete(request, id):
+    student = Student.objects.get(id=id)
+    student.delete()
+    return redirect("/all-students")
