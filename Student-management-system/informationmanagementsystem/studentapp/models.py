518cf30f4778e90b6b6a7be7627278920f6f5d5a
Ataullah Behesti <ataullah.behesti@gmail.com>
2022-12-07 12:04:35 +0200

    Django model

--- Student-management-system/informationmanagementsystem/studentapp/models.py
+++ Student-management-system/informationmanagementsystem/studentapp/models.py
@@ -7,6 +7,7 @@ class Student(models.Model):
     std_name = models.CharField(max_length=200)
     std_email = models.EmailField()
     std_contact_number = models.CharField(max_length=15)
+    std_birth_date = models.DateField()
 
     class Meta:
         db_table = "student"
